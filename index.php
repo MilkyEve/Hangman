a79d28706aeaed904403a32c963401728542e754
MilkyEve <559324@edu.rocmn.nl>
2023-05-10 11:17:55 +0200

    asdf

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-blehh :pp
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hangman</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Hangman</h1>
+    <div id="outerdiv">
+        <div><a href="#"><span class="homepage-button">Start with a random word</span></a></div>
+        <div><img src="hangman.png" alt=""></div>
+        <div><a href="#"><span class="homepage-button">Start with a custom word</span></a></div>
+    </div>
+</body>
+</html>
